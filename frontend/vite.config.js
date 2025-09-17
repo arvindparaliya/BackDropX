e284d029eb1f65d3050b165d6499566ea72a077f
arvindparaliya <arvindparaliya18@gmail.com>
2025-09-17 11:42:35 +0530

    BackDropX - frontend add

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@vitejs/plugin-react";
 
-// configuration of Vite
+// Export Vite configuration with React and Tailwind plugins
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 });
