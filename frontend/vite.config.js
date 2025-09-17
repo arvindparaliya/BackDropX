b13ad0bab9369c37d1deeebc0aa47ab0ce1175a2
arvindparaliya <arvindparaliya18@gmail.com>
2025-09-17 11:38:52 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// configuration of Vite
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 });
